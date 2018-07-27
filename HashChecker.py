650f4a89b953b6d1ab8a4cda387f5ef2400239c3
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2018-07-27 23:58:13 +0600

    updated

--- HashChecker.py
+++ HashChecker.py
@@ -2,18 +2,46 @@
 
 
 def checkHash(fileLoc, option):
+
     # Hash calculator
-    import hashlib
+    from hashlib import md5, sha256, sha512
+
+    # Specifing how many bytes of the file to open at a time
+    BLOCKSIZE = 128000
+    # 128000 Byte = 128 KiloByte
+
+
+    # Variables
+    md5Hasher = md5()
+    sha256Hasher = sha256()
+    sha512Hasher = sha512()
+
     with open(fileLoc, 'rb') as file:
-        fileData = file.read()
-    if(option == 'md5'):
-        return hashlib.md5(fileData).hexdigest()
-    elif(option == 'sha256'):
-        return hashlib.sha256(fileData).hexdigest()
-    elif(option == 'sha512'):
-        return hashlib.sha512(fileData).hexdigest()
-    else:
-        return -1
+        fileData = file.read(BLOCKSIZE)
+
+        # Conditions for desired hashing
+
+        if(option == 'md5'): # MD5sum
+            while(len(fileData) > 0):
+                md5Hasher.update(fileData)
+                fileData = file.read(BLOCKSIZE)
+            return md5Hasher.hexdigest()
+
+        elif(option == 'sha256'): # SHA256sum
+            while(len(fileData) > 0):
+                sha256Hasher.update(fileData)
+                fileData = file.read(BLOCKSIZE)
+            return sha256Hasher.hexdigest()
+
+        elif(option == 'sha512'): # SHA512sum
+            while(len(fileData) > 0):
+                sha512Hasher.update(fileData)
+                fileData = file.read(BLOCKSIZE)
+            return sha512Hasher.hexdigest()
+
+        else:
+            return -1
+
 
 
 if __name__ == '__main__':
