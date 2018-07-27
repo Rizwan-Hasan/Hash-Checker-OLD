ab4b6868422654772a8d686f4a2be7bdf361c29a
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2018-07-27 22:05:28 +0600

    updated

---
+++ launcher.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+    char appMain[] = "/run/media/rizwan/Work/Work/Hash-Checker-Dev/main.py";
+    char launcher[100];
+
+    if(argc == 2) {
+        sprintf(launcher, "python3 %s %s", appMain, argv[1]);
+        printf("%s", argv[1]);
+        system(launcher);
+    }
+    else {
+        sprintf(launcher, "python3 %s", appMain);
+        system(launcher);
+    }
+    return 0;
+}
