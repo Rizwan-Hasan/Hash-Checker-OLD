b9291743bdca934f554dd849fb79bba01827304b
Rizwan Hasan <rizwan.hasan486@gmail.com>
2018-08-12 01:44:00 +0600

    updated

--- filesMD5.py
+++ filesMD5.py
@@ -5,10 +5,10 @@
 default_css = 'efb4b1de53d12ee86ff3753ac39e4439'
 
 # MD5sum of file 'ui/MainWindow.ui' ↓
-MainWindow_ui = '4e5f10da2920e8bad05369808617c6a6'
+MainWindow_ui = '587385ea76685ffa2e65661623c787f9'
 
 # MD5sum of file 'ui/AboutWindow.ui' ↓
-AboutWindow_ui = '191911a4a8348aad33da4eef41d59fa4'
+AboutWindow_ui = '9c75f6a82a247264859643c08d6b348f'
 
 
 if __name__ == '__main__':
--- versionInfo.py
+++ versionInfo.py
@@ -1,14 +1,26 @@
+import os
+import platform
+
 # Hash Checker Software Version ↓
 
 version = 'Version: 2.1'
 
-Software_Version = """
-    <font style="font-size: 10pt;
-                 font-weight: 496;
-                 color: #3e3e3e;">
-        <b>""" + version + """</b>
-    <font>
-"""
+if (platform.system() == 'Windows'):
+    Software_Version = """
+        <font style="font-size: 8pt;
+                     font-weight: 496;
+                     color: #3e3e3e;">
+            <b>""" + version + """</b>
+        <font>
+    """
+else:
+    Software_Version = """
+        <font style="font-size: 10pt;
+                     font-weight: 496;
+                     color: #3e3e3e;">
+            <b>""" + version + """</b>
+        <font>
+    """
 
 if __name__ == '__main__':
     print(version)
