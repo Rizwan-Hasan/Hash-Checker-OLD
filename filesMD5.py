5096c4ff1266dd5ac66cb83d0730c178e680a7ea
Rizwan Hasan <rizwan.hasan486@gmail.com>
2018-08-14 19:08:13 +0600

    updated

--- filesMD5.py
+++ filesMD5.py
@@ -2,13 +2,13 @@
 
 
 # MD5sum of file 'ui/default.css' ↓
-default_css = '93ea9e089c33850a50bf02efb9d4a0bc'
+default_css = 'efb4b1de53d12ee86ff3753ac39e4439'
 
 # MD5sum of file 'ui/MainWindow.ui' ↓
-MainWindow_ui = '290d4bea82a50a1b78a702eda2d5da1b'
+MainWindow_ui = '4e5f10da2920e8bad05369808617c6a6'
 
 # MD5sum of file 'ui/AboutWindow.ui' ↓
-AboutWindow_ui = '578dc37f2d03f5c9b9b722a8b490cfdc'
+AboutWindow_ui = '191911a4a8348aad33da4eef41d59fa4'
 
 
 if __name__ == '__main__':
